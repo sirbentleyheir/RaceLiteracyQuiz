bf651c7a0f43f8038a43535042a4f2544adc48e3
sirjahibentley® <info@sirjahibentley.com>
2022-09-10 21:31:39 +0200

    init

---
+++ js/questions.js
@@ -0,0 +1,185 @@
+//creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+        numb: 1,
+        question: "Race is identified by:",
+        answer: "None of the above",
+        options: [
+            "Blood",
+            "Skin",
+            "Ancestry",
+            "Genes",
+            "All of the above",
+            "None of the above"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Which species has the greatest genetic diversity?",
+        answer: "Fruit Flies",
+        options: [
+            "Humans",
+            "Chimpanzees",
+            "Penguins",
+            "Fruit Flies",
+            "Elephants",
+            "Dogs"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Which two populations have the highest average likelihood of sharing genetic traits?",
+        answer: "Saudi Arabians and Ethiopians",
+        options: [
+            "Italians and Ethiopians",
+            "Senegalese and Kenyans",
+            "Italians and Swedes",
+            "Chinese and Lakota(Sioux)",
+            "Saudi Arabians and Ethiopians",
+            "Germans and Russians"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Which continent has the most genetic variety among people?",
+        answer: "Africa",
+        options: [
+            "Europe",
+            "Asia",
+            "South America",
+            "Africa",
+            "North America",
+            "Antartica"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Greeks believed that the following traits set them apart from 'barbarians':",
+        answer: "Language",
+        options: [
+            "Religion",
+            "Skin",
+            "Language",
+            "Customs",
+            "Hairiness",
+            "Intelligence"
+        ]
+    },
+    {
+        numb: 6,
+        question: "John Rolfe, a colonist from Jamestown, and his new bride Pocahontas made headlines in 1619 when they visited the Court of London for the following reasons:",
+        answer: "A princess married beneath her status by marrying a commoner",
+        options: [
+            "An Englishman had married an Indian",
+            "A Christian had married a heathen",
+            "A princess married beneath her status by marrying a commoner",
+            "Londoners had never seen an Indian before",
+            "Pocahontas helped struggling English colonist survive in Virginia",
+            "Pocahontas was Powhatan Queen"
+        ]
+    },
+    {
+        numb: 7,
+        question: "Which of the following did NOT play a significant role in how African slavery originally spread over North America?",
+        answer: "They were deemed innately inferior",
+        options: [
+            "As non-Christians, they had no legal protections",
+            "They were skilled semi-tropical farmers",
+            "The supply of indentured servants from Europe was becoming unreliable",
+            "They were deemed innately inferior",
+            "Unlike Native Americans, they were resistant to Europeans diseases",
+            "They couldn't easily run away"
+        ]
+    },
+    {
+        numb: 8,
+        question: "The factors directly responsible for the birth of the concept of white supremacy in America were:",
+        answer: "Declaration of Independence",
+        options: [
+            "Indian removal",
+            "Slavery",
+            "Declaration of Independence",
+            "The U.S. Constitution",
+            "Ancient Greece",
+            "Education"
+        ]
+    },
+    {
+        numb: 9,
+        question: "Which was NOT a white person's introduction to an Indian?",
+        answer: "Democracy",
+        options: [
+            "An Indian identity",
+            "Democracy",
+            "Identity by blood quantum",
+            "Horses",
+            "Measles",
+            "Christopher Columbus"
+        ]
+    },
+    {
+        numb: 10,
+        question: "What proportion of the $120 billion in house loans gauranteed by the federal government between 1933 and 1962 went to homeowners of European descent?",
+        answer: "98%",
+        options: [
+            "45%",
+            "64%",
+            "75%",
+            "88%",
+            "92%",
+            "98%"
+        ]
+    },
+    {
+        numb: 11,
+        question: "Which of the following is NOT a result of federal government policies?",
+        answer: "Affirmative action quotas",
+        options: [
+            "Redlining",
+            "Urban renewal",
+            "Deterioration of inner cities",
+            "Affirmative action quotas",
+            "The wealth gap between black and white families",
+            "Prison Industrial Complex"
+        ]
+    },
+    {
+        numb: 12,
+        question: "Today, the net worth of the average white family is how much compared to the average Black family?",
+        answer: "Eight times as much",
+        options: [
+            "Three times as much",
+            "Eight times as much",
+            "A third as much",
+            "Half as much",
+            "Twice as much",
+            "The same"
+        ]
+    },
+    {
+        numb: 13,
+        question: "Accordingly to a 1993 study, 86% of suburban whites lived in a community where the Black population was:",
+        answer: "Less than 1%",
+        options: [
+            "Less than 5%",
+            "Less than 10%",
+            "Less than 1%",
+            "More than 10%",
+            "More than 15%",
+            "More than 20%"
+        ]
+    },
+    {
+        numb: 14,
+        question: "Which is NOT an example of a government racial preference program?",
+        answer: "1964 Civil Rights Act",
+        options: [
+            "1964 Civil Rights Act",
+            "1862 Homestead Act",
+            "1790 Naturalization Act",
+            "1934 Federal Housing Administration",
+            "1935 Social Security Act",
+            "1972 Equal Right Amendment"
+        ]
+    },
+];
